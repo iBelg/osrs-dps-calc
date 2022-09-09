e09aa76a84ee364209e895b98b53d18880964e68
iBelg <4256757+iBelg@users.noreply.github.com>
2022-09-09 17:53:09 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="h-screen flex flex-col bg-slate-800 text-white">
+    <header class="py-3 px-4 text-lg font-medium">Old School RuneScape DPS Calculator</header>
+    <div class="flex h-full">
+      <nav class="flex flex-col items-center w-16 p-2">
+        <span>LINK</span>
+      </nav>
+      <main class="w-full h-full p-2 bg-slate-600">MAIN</main>
+    </div>
+  </div>
+</template>
